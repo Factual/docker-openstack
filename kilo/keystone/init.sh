9515f0775eac9013fc0105bc70248ae0a68084fe
Maxime Devalland <maxime@factual.com>
2015-08-19 14:30:09 -0700

    fix on keystone init.sh

--- kilo/keystone/init.sh
+++ kilo/keystone/init.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 OS_KEYSTONE_ADMIN_TOKEN=${OS_KEYSTONE_ADMIN_TOKEN:-ADMIN}
-sed -i "s/#admin_token=ADMIN/admin_token=$OS_KEYSTONE_ADMIN_TOKEN/g" /etc/keystone/keystone.conf
+sed -i "s/#admin_token.*/admin_token=$OS_KEYSTONE_ADMIN_TOKEN/g" /etc/keystone/keystone.conf
