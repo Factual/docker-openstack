5b9675dd1731ab803f13ff03f9e1bc6e74e8f8e0
Maxime Devalland <maxime@factual.com>
2015-08-19 17:22:28 -0700

    fix database issue

--- kilo/database/mariadb.sh
+++ kilo/database/mariadb.sh
@@ -7,7 +7,7 @@ if [[ ! -d $VOLUME_HOME/mysql ]]; then
     echo "=> Installing MariaDB ..."
     mysql_install_db > /dev/null 2>&1
     echo "=> Done!"  
-    /create_mariadb_admin_user.sh
+    /etc/my_init.d/00_create_mariadb_admin_user.sh
 else
     echo "=> Using an existing volume of MariaDB"
 fi
