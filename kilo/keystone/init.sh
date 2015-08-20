dd0bcfc39af33b4075b3758a20ad25c0c397f2e4
Maxime Devalland <maxime@factual.com>
2015-08-20 11:26:26 -0700

    fixed typo in keystone init script

--- kilo/keystone/init.sh
+++ kilo/keystone/init.sh
@@ -5,7 +5,7 @@ OS_KEYSTONE_DB_PASSWORD=${OS_KEYSTONE_DB_PASSWORD:-keystone}
 OS_DB_URL=${OS_DB_URL:-controller}
 
 sed -i "s/#admin_token.*/admin_token=$OS_KEYSTONE_ADMIN_TOKEN/g" /etc/keystone/keystone.conf
-sed -i "s|connection = sqlite.*|mysql://keystone:$OS_KEYSTONE_DB_PASSWORD@$OS_DB_URL/keystone|g" /etc/keystone/keystone.conf
+sed -i "s|connection = sqlite.*|connection = mysql://keystone:$OS_KEYSTONE_DB_PASSWORD@$OS_DB_URL/keystone|g" /etc/keystone/keystone.conf
 
 # Download Virtulhost
 wget -q http://resources.prod.factual.com/services/openstack/keystone/wsgi-keystone.conf -O /etc/apache2/sites-available/wsgi-keystone.conf
